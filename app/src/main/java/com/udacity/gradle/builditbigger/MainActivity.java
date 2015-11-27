e7595686ace01acdad65fd4c46ca439384c3967c
Jill Hickman <jillhickman@gmail.com>
2015-11-27 15:17:21 -0700

    Added the jokegeneratorlibrary java Library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokeGenerator;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeGenerator myJokeGenerator = new JokeGenerator();
+
+        Toast.makeText(this, myJokeGenerator.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
